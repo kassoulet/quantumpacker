4a0c92beac74e56ceffef8421943bd2bc20e51ba
gautier <gautier.portet@vegatechnologies.fr>
2010-05-03 13:22:18 +0200

    Add docstrings.

--- qp.py
+++ qp.py
@@ -8,25 +8,33 @@ using quantum chaos applied to topological quantum numbers.
 quantumpacker can compress huge files to only a series of topological quantum numbers,
 resulting in a file 50x smaller. (51.113x in fact)
 
-The method is complex, but the compression is reasonably fast, and decompression is 
+The method is complex, but the compression is reasonably fast, and decompression is
 even faster.
 """
 
 print 'quantumpacker - Quantum Quantization Compressor - by Gautier Portet <kassoulet at gmail>'
-
-def error(msg):
-    print msg
-    raise SystemExit
 
 import os
 import sys
 import time
 from os.path import getsize
 
+# compression buffer size
 BLOCK_SIZE = 1024*1
+
+def error(msg):
+    """
+    Print an error message and exit program.
+    """
+    print msg
+    raise SystemExit
+
 
 progress_next = 0
 def progress(filename, progress):
+    """
+    Display current processed filename, and job progress (in percent).
+    """
     global progress_next
     t = time.time()
     if t > progress_next:
@@ -36,12 +44,19 @@ def progress(filename, progress):
 
 
 def is_quantum_file(filename):
+    """
+    Return True if the given filename is a file compressed by quantumpacker.
+    """
     marker = 'q1'
     b = open(filename).read(len(marker))
     return b == marker
 
 
 def compress(filename):
+    """
+    Compress a file.
+    Will create a file with '.quant' prefixed to the filename.
+    """
     quantizer = QuantumQuantizer()
 
     f = open(filename, 'rb')
@@ -65,6 +80,10 @@ def compress(filename):
 
 
 def decompress(filename):
+    """
+    Decompress a file.
+    Will create a file with '.orig' prefixed to the filename.
+    """
     expander = QuantumExpander(filename)
 
     f = open(filename+'.orig', 'wb')
@@ -83,18 +102,21 @@ def decompress(filename):
     print filename, 'ok  '
 
 
-
 def manage_file(filename):
+    """
+    If the given file is compressed by quantumpacker, decompress. Else compress.
+    """
     if is_quantum_file(filename):
         decompress(filename)
     else:
         compress(filename)
 
+
 from core import QuantumQuantizer, QuantumExpander
 
 files = sys.argv[1:]
 if not files:
-    error('need files to compress...')
+    error('need files to compress or decompress...')
 
 for f in files:
     manage_file(f)
